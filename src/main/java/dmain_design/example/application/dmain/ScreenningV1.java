6d2419b206aab8da70beb74a4c41cad29d7101a0
宇都宮靖之 <csya18003@g.nihon-u.ac.jp>
2020-03-04 19:43:59 -0800

    ドメイン駆動処理の設定

---
+++ src/main/java/dmain_design/example/application/dmain/ScreenningV1.java
@@ -0,0 +1,12 @@
+package dmain_design.example.application.dmain;
+
+@Getter
+@Setter
+public class ScreenningV1{
+    //採用選考ID
+    private String screeningId;
+
+    //応募した日
+    private Localdate applyDate;
+
+}
